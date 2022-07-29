1ce27aa06bff4dd54d81f6f2bf3239a2ba858d78
Winniee356 <lamwingjie@gmail.com>
2022-07-29 11:34:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // items 
+const menu = [
+ {
+    id: 1,
+    title:"Matcha latte",
+    category:"drinks",
+    price:4,
+    img:"./images/vegan-matchalatte.jpg",
+    desc: `Green tea matcha with dairy-free milk alternatives. Choose either oat, soy or coconut milk`,
+ },
+
+]
