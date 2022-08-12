9c948f1fd399bc01ffd834aad65081b412570138
Winniee356 <lamwingjie@gmail.com>
2022-08-12 20:01:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const menu = [
    id: 3,
    title:"Fluffy Pancake",
    category:"brunch",
-   price:7,50,
+   price:7.50,
    img:"./images/vegan-pancake.png",
    desc: `Fluffy vegan pancake made with oatmilk and vegan butter. Topped with strawberry and blueberry. Option vegan cream and agave syrup toppings.`,
 },
@@ -28,7 +28,7 @@ const menu = [
    id: 4,
    title:"Vanilla French Toast",
    category:"brunch",
-   price:9,50,
+   price:9.50,
    img:"./images/vegan-frenchtoast.jpg",
    desc: `Golden brown vegan french toast topped with vegan ice cream. Optional toppings: vegan cream and agave syrup toppings.`,
 },
@@ -52,7 +52,7 @@ const menu = [
    id: 7,
    title:"Banana donut",
    category:"dessert",
-   price:3,50,
+   price:3.50,
    img:"./images/vegan-bananadonuts.jpg",
    desc: `Vegan banana donut with dark chocolate and vegan cream. `,
 },
@@ -68,7 +68,7 @@ const menu = [
    id: 9,
    title:"Vegan Oreo cake",
    category:"dessert",
-   price:3,50,
+   price:3.50,
    img:"./images/vegan-oreocake.jpg",
    desc: `Oreo baked vegan cheesecake made from cashews nuts and dark chocolate ganache. `,
 },
@@ -77,5 +77,5 @@ const menu = [
 const sectionCenter = document.querySelector(`.section-center`);
 
 window.addEventListener(`DOMContentLoaded`,function(){
-   
+   console.log("Hello");
 })
