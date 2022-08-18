cd0c6eb79941a17c25b139d0dc7e68539fa3eb37
Winniee356 <lamwingjie@gmail.com>
2022-08-18 18:59:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -77,24 +77,7 @@ const menu = [
 const sectionCenter = document.querySelector(`.section-center`);
 
 window.addEventListener(`DOMContentLoaded`,function(){
-   let displayMenu = menu.map(function(item){
-      //console.log(item);
-
-   return `  <article class="menu-item">
-   <img src=${item.img} class="photo" alt=${item.title} />
-   <div class="item-info">
-       <header>
-           <h4>${item.title}</h4>
-           <h4 class="price">€${item.price}</h4>
-       </header>
-       <p class="item-text">
-       ${item.desc}
-       </p>
-   </div>
-</article>`;
-   });
-   displayMenu = displayMenu.join("");
-   sectionCenter.innerHTML = displayMenu;
+  displayMenuItems(menu)
 });
 
 function displayMenuItems(menuItems) {
