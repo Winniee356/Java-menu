8e253c1d8331b3a87b33bbfe6a49793d667f2984
Winniee356 <lamwingjie@gmail.com>
2022-08-18 18:56:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -95,4 +95,25 @@ window.addEventListener(`DOMContentLoaded`,function(){
    });
    displayMenu = displayMenu.join("");
    sectionCenter.innerHTML = displayMenu;
-})
+});
+
+function displayMenuItems(menuItems) {
+   let displayMenu = menu.map(function(item){
+      //console.log(item);
+
+   return `  <article class="menu-item">
+   <img src=${item.img} class="photo" alt=${item.title} />
+   <div class="item-info">
+       <header>
+           <h4>${item.title}</h4>
+           <h4 class="price">€${item.price}</h4>
+       </header>
+       <p class="item-text">
+       ${item.desc}
+       </p>
+   </div>
+</article>`;
+   });
+   displayMenu = displayMenu.join("");
+   sectionCenter.innerHTML = displayMenu;
+}
