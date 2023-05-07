41b406cee0dd75eeca092e504163b295127e38ed
Winniee356 <lamwingjie@gmail.com>
2023-05-07 10:20:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const menu = [
    category:"Brunch",
    price:9.50,
    img:"./images/vegan-frenchtoast.jpg",
-   desc: `Vegan french toast toasted golden brown topped with vegan ice cream. Optional toppings: vegan cream and agave syrup toppings.`,
+   desc: `Vegan french toast bread toasted golden brown topped with vegan ice cream and fruits. Optional toppings: vegan cream and agave syrup toppings.`,
 },
 {
    id: 5,
