195db1ac4c74a405954b6ab796c9037e3e05c04e
Winniee356 <lamwingjie@gmail.com>
2023-05-16 11:56:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const menu = [
    category:"Drinks",
    price:2.50,
    img:"./images/vegan-strawberrymilk.jpg",
-   desc: `Fresh Vegan strawberry milk is made of fresh organic strawberry jam handmade with oatmilk and a dash of vanilla extract. Perfect afternoon pairing with a slice of cake. `,
+   desc: `Strawberry milk is made of fresh organic strawberry jam handmade with oatmilk and a dash of vanilla extract. Perfect afternoon pairing with a slice of cake. `,
 },
 {
    id: 11,
